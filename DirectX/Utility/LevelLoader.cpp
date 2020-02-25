03c252dbeb153fccb23fcac866c6cfa3f2182ae1
takano <llmn.0419@gmail.com>
2020-02-26 02:18:18 +0900

    個別のアクターを読み込めるように変更

--- DirectX/Utility/LevelLoader.cpp
+++ DirectX/Utility/LevelLoader.cpp
@@ -34,7 +34,7 @@ LevelLoader::LevelLoader() {
 
 LevelLoader::~LevelLoader() = default;
 
-bool LevelLoader::loadGlobal(std::shared_ptr<Renderer> renderer, const std::string & fileName) {
+bool LevelLoader::loadGlobal(std::shared_ptr<Renderer> renderer, const std::string & fileName) const {
     rapidjson::Document doc;
     if (!loadJSON(fileName, &doc)) {
         MSG(L"レベルファイルのロードに失敗しました");
@@ -55,7 +55,7 @@ bool LevelLoader::loadGlobal(std::shared_ptr<Renderer> renderer, const std::stri
     return true;
 }
 
-bool LevelLoader::loadActors(std::shared_ptr<Renderer> renderer, const std::string & fileName) {
+bool LevelLoader::loadActors(std::shared_ptr<Renderer> renderer, const std::string & fileName) const {
     rapidjson::Document doc;
     if (!loadJSON(fileName, &doc)) {
         MSG(L"レベルファイルのロードに失敗しました");
@@ -70,7 +70,23 @@ bool LevelLoader::loadActors(std::shared_ptr<Renderer> renderer, const std::stri
     return true;
 }
 
-bool LevelLoader::loadJSON(const std::string & fileName, rapidjson::Document * outDoc) {
+std::shared_ptr<Actor> LevelLoader::loadSpecifiedActor(std::shared_ptr<Renderer> renderer, const std::string& fileName, const std::string& type) const {
+    rapidjson::Document doc;
+    if (!loadJSON(fileName, &doc)) {
+        MSG(L"レベルファイルのロードに失敗しました");
+        return nullptr;
+    }
+
+    std::shared_ptr<Actor> actor = nullptr;
+    const rapidjson::Value& actors = doc["actors"];
+    if (actors.IsArray()) {
+        actor = loadSpecifiedActorProperties(renderer, actors, type);
+    }
+
+    return actor;
+}
+
+bool LevelLoader::loadJSON(const std::string & fileName, rapidjson::Document * outDoc) const {
     //フォルダ階層の移動
     setDataDirectory();
 
@@ -101,7 +117,7 @@ bool LevelLoader::loadJSON(const std::string & fileName, rapidjson::Document * o
     return true;
 }
 
-void LevelLoader::loadGlobalProperties(std::shared_ptr<Renderer> renderer, const rapidjson::Value & inObject) {
+void LevelLoader::loadGlobalProperties(std::shared_ptr<Renderer> renderer, const rapidjson::Value & inObject) const {
     //環境光を取得
     Vector3 ambient;
     if (JsonHelper::getVector3(inObject, "ambientLight", &ambient)) {
@@ -117,58 +133,103 @@ void LevelLoader::loadGlobalProperties(std::shared_ptr<Renderer> renderer, const
     }
 }
 
-void LevelLoader::loadActorsProperties(std::shared_ptr<Renderer> renderer, const rapidjson::Value & inArray) {
+void LevelLoader::loadActorsProperties(std::shared_ptr<Renderer> renderer, const rapidjson::Value & inArray) const {
     //アクターの配列をループ
     for (rapidjson::SizeType i = 0; i < inArray.Size(); i++) {
+        //有効なオブジェクトか
         const rapidjson::Value& actorObj = inArray[i];
         if (!actorObj.IsObject()) {
             continue;
         }
-        //型名を取得
+        //有効な型名か
         std::string type;
-        if (JsonHelper::getString(actorObj, "type", &type)) {
-            auto iter = mActors.find(type);
-            if (iter != mActors.end()) {
-                //mapからアクターを生成
-                auto actor = iter->second(renderer, actorObj["properties"]);
-                //コンポーネントプロパティがあれば取得
-                if (actorObj.HasMember("components")) {
-                    const rapidjson::Value& components = actorObj["components"];
-                    if (components.IsArray()) {
-                        loadComponents(actor, components);
-                    }
-                }
-            } else {
-                MSG(L"このアクターは存在しません");
-            }
+        if (!JsonHelper::getString(actorObj, "type", &type)) {
+            continue;
+        }
+        //mapに存在するか
+        auto iter = mActors.find(type);
+        if (iter == mActors.end()) {
+            continue;
+        }
+        //mapからアクターを生成
+        auto actor = iter->second(renderer, actorObj["properties"]);
+        //コンポーネントプロパティがあれば取得
+        if (!actorObj.HasMember("components")) {
+            continue;
+        }
+        const rapidjson::Value& components = actorObj["components"];
+        if (components.IsArray()) {
+            loadComponents(actor, components);
         }
     }
 }
 
-void LevelLoader::loadComponents(std::shared_ptr<Actor> actor, const rapidjson::Value& inArray) {
+std::shared_ptr<Actor> LevelLoader::loadSpecifiedActorProperties(std::shared_ptr<Renderer> renderer, const rapidjson::Value& inArray, const std::string& type) const {
+    //そもそも指定のtypeが存在しているかチェック
+    auto itr = mActors.find(type);
+    if (itr == mActors.end()) {
+        return nullptr;
+    }
+
+    std::shared_ptr<Actor> actor = nullptr;
+    //アクターの配列をループ
+    for (rapidjson::SizeType i = 0; i < inArray.Size(); i++) {
+        //有効なオブジェクトか
+        const rapidjson::Value& actorObj = inArray[i];
+        if (!actorObj.IsObject()) {
+            continue;
+        }
+        //有効な型名か
+        std::string t;
+        if (!JsonHelper::getString(actorObj, "type", &t)) {
+            continue;
+        }
+        //指定のタイプと一致するか
+        if (t != type) {
+            continue;
+        }
+        //mapからアクターを生成
+        actor = itr->second(renderer, actorObj["properties"]);
+        //コンポーネントプロパティがあれば取得
+        if (!actorObj.HasMember("components")) {
+            break;
+        }
+        const rapidjson::Value& components = actorObj["components"];
+        if (components.IsArray()) {
+            loadComponents(actor, components);
+            break;
+        }
+    }
+
+    return actor;
+}
+
+void LevelLoader::loadComponents(std::shared_ptr<Actor> actor, const rapidjson::Value & inArray) const {
     //コンポーネントの配列をループ
     for (rapidjson::SizeType i = 0; i < inArray.Size(); i++) {
+        //有効なオブジェクトか
         const rapidjson::Value& compObj = inArray[i];
         if (!compObj.IsObject()) {
             continue;
         }
-        //型名を取得
+        //有効な型名か
         std::string type;
-        if (JsonHelper::getString(compObj, "type", &type)) {
-            auto iter = mComponents.find(type);
-            if (iter != mComponents.end()) {
-                //アクターがそのコンポーネントを保持しているか
-                auto comp = actor->componentManager()->getComponent(iter->first);
-                if (comp) {
-                    //プロパティをロード
-                    comp->loadProperties(compObj["properties"]);
-                } else {
-                    //新規コンポーネントを生成
-                    comp = iter->second(actor, compObj["properties"]);
-                }
-            } else {
-                MSG(L"このコンポーネントは存在しません");
-            }
+        if (!JsonHelper::getString(compObj, "type", &type)) {
+            continue;
+        }
+        //mapに存在するか
+        auto iter = mComponents.find(type);
+        if (iter == mComponents.end()) {
+            continue;
+        }
+        //アクターがそのコンポーネントを保持しているか
+        auto comp = actor->componentManager()->getComponent(iter->first);
+        if (comp) {
+            //プロパティをロード
+            comp->loadProperties(compObj["properties"]);
+        } else {
+            //新規コンポーネントを生成
+            comp = iter->second(actor, compObj["properties"]);
         }
     }
 }
--- DirectX/Utility/LevelLoader.h
+++ DirectX/Utility/LevelLoader.h
@@ -12,30 +12,34 @@ class Actor;
 class Component;
 class Renderer;
 
-using ActorFunc = std::function<std::shared_ptr<Actor>(std::shared_ptr<Renderer>, const rapidjson::Value&)>;
-using ComponentFunc = std::function<std::shared_ptr<Component>(std::shared_ptr<Actor>, const rapidjson::Value&)>;
-
 class LevelLoader {
     friend class Singleton<LevelLoader>;
 
+    using ActorFunc = std::function<std::shared_ptr<Actor>(std::shared_ptr<Renderer>, const rapidjson::Value&)>;
+    using ComponentFunc = std::function<std::shared_ptr<Component>(std::shared_ptr<Actor>, const rapidjson::Value&)>;
+
 public:
     //グローバルデータを読み込む
-    bool loadGlobal(std::shared_ptr<Renderer> renderer, const std::string& fileName);
+    bool loadGlobal(std::shared_ptr<Renderer> renderer, const std::string& fileName) const;
     //アクターを読み込む
-    bool loadActors(std::shared_ptr<Renderer> renderer, const std::string& fileName);
+    bool loadActors(std::shared_ptr<Renderer> renderer, const std::string& fileName) const;
+    //指定のアクターを読み込む
+    std::shared_ptr<Actor> loadSpecifiedActor(std::shared_ptr<Renderer> renderer, const std::string& fileName, const std::string& type) const;
 
 private:
     LevelLoader();
     ~LevelLoader();
 
     //jsonファイルの読み込み
-    bool loadJSON(const std::string& fileName, rapidjson::Document* outDoc);
+    bool loadJSON(const std::string& fileName, rapidjson::Document* outDoc) const;
     //グローバルプロパティの読み込み
-    void loadGlobalProperties(std::shared_ptr<Renderer> renderer, const rapidjson::Value& inObject);
+    void loadGlobalProperties(std::shared_ptr<Renderer> renderer, const rapidjson::Value& inObject) const;
     //アクターの読み込み
-    void loadActorsProperties(std::shared_ptr<Renderer> renderer, const rapidjson::Value& inArray);
+    void loadActorsProperties(std::shared_ptr<Renderer> renderer, const rapidjson::Value& inArray) const;
+    //指定のアクターの読み込み
+    std::shared_ptr<Actor> loadSpecifiedActorProperties(std::shared_ptr<Renderer> renderer, const rapidjson::Value& inArray, const std::string& type) const;
     //コンポーネントの読み込み
-    void loadComponents(std::shared_ptr<Actor> actor, const rapidjson::Value& inArray);
+    void loadComponents(std::shared_ptr<Actor> actor, const rapidjson::Value& inArray) const;
 
 private:
     static constexpr int LEVEL_VERSION = 1;
