6ccf97949bad7fa8ecf76bd50cc9e937728154db
takano <llmn.0419@gmail.com>
2020-04-05 23:47:18 +0900

    ObserverとSubject作ってみた

---
+++ DirectX/Event/IObserver.h
@@ -0,0 +1,7 @@
+﻿#pragma once
+
+class IObserver {
+public:
+    virtual ~IObserver() = default;
+    virtual void onNotify() = 0;
+};
---
+++ DirectX/Event/Subject.cpp
@@ -0,0 +1,25 @@
+﻿#include "Subject.h"
+#include "IObserver.h"
+
+Subject::Subject() = default;
+
+Subject::~Subject() = default;
+
+void Subject::addObserver(const ObserverPtr& observer) {
+    mObservers.emplace_back(observer);
+}
+
+void Subject::removeObserver(const ObserverPtr& observer) {
+    for (auto itr = mObservers.begin(); itr != mObservers.end(); ++itr) {
+        if (*itr == observer) {
+            mObservers.erase(itr);
+            return;
+        }
+    }
+}
+
+void Subject::notify() {
+    for (auto&& observer : mObservers) {
+        observer->onNotify();
+    }
+}
